7ea775e21889e94c59c9319ca52897be875925ef
InnovateBham_8 <innovatebham_8@UABiLAB-8s-iMac.local>
2017-06-21 10:53:32 -0500

    finally fixed box-button issu

--- squareslab.js
+++ squareslab.js
@@ -6,10 +6,47 @@
 //     document.body.appendChild(x);
 // }
 
-var x = document.getElementById("myBtn");
-function myFunction () {
+
+// document.addEventListener("DOMContentLoaded", function(){
+//     var x = document.createElement('button');
+//     x.addEventListener('click', myDiv);
+//     function myFunction(
+//        function myDiv(){
+//     var myDiv = document.createElement('div');
+//     myDiv.className='Div1';
+//     myDiv.id = "divId"
+//     x.appendChild(myDiv);
+//     document.body.appendChild(x);
+// });
+
+// document.addEventListener('DOMContentLoaded', function() {
+//        var x = document.createElement('Button');
+//        button.addEventListener('imPRESS ME', pleaseJustWork);
+// });
+
+
+
+// function pleaseJustWork() {
+//     var x = document.createElement('Button');
+//     var t = document.createTextNode('Click me');
+//     var belleDivDevoe =  document.createElement('div');
+//     belleDivDevoe.style.backgroundColor = 'black';
+//     belleDivDevoe.style.height = '80px';
+//     belleDivDevoe.style.width = '80px';
+//     x.appendChild(belleDivDevoe);
+//     document.body.appendChild(x);
+// }     
+
+function pleaseJustWork() {
     var x = document.createElement('Button');
-    var myDiv = document.createElement('div');
-    x.appendChild(myDiv);
-    document.body.appendChild(x);
-}
+    var t = document.createTextNode('Click me');
+    var belleDivDevoe =  document.createElement('div');
+    belleDivDevoe.style.backgroundColor = 'black';
+    belleDivDevoe.style.height = '80px';
+    belleDivDevoe.style.width = '80px';
+    belleDivDevoe.style.float = "left";
+    x.appendChild(belleDivDevoe);
+    document.body.appendChild(belleDivDevoe);
+    belleDivDevoe.appendChild(t);
+
+}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
