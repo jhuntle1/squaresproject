e12e4e3f30ebc06639a0fc04beba2d700791a60d
InnovateBham_8 <innovatebham_8@UABiLAB-8s-iMac.local>
2017-06-22 09:01:46 -0500

    final

--- squareslab.js
+++ squareslab.js
@@ -1,52 +1,88 @@
-// var x = document.getElementById("myBtn");
-// function myFunction () {
-//     var x = document.createElement('Button');
-//     var t = document.createTextNode('Click me');
-//     x.appendChild(t);
-//     document.body.appendChild(x);
-// }
-
-
-// document.addEventListener("DOMContentLoaded", function(){
-//     var x = document.createElement('button');
-//     x.addEventListener('click', myDiv);
-//     function myFunction(
-//        function myDiv(){
-//     var myDiv = document.createElement('div');
-//     myDiv.className='Div1';
-//     myDiv.id = "divId"
-//     x.appendChild(myDiv);
-//     document.body.appendChild(x);
-// });
-
-// document.addEventListener('DOMContentLoaded', function() {
-//        var x = document.createElement('Button');
-//        button.addEventListener('imPRESS ME', pleaseJustWork);
-// });
-
-
 
 // function pleaseJustWork() {
 //     var x = document.createElement('Button');
-//     var t = document.createTextNode('Click me');
-//     var belleDivDevoe =  document.createElement('div');
+//     var i = 1;
+
+//     var belleDivDevoe = document.createElement('div');
+//     document.body.appendChild(belleDivDevoe);
+//     belleDivDevoe.id = i;
+//     i++;
+
+//     belleDivDevoe.addEventListener('mouseover', function () {
+//         belleDivDevoe.innerHTML = belleDivDevoe.id;
+//     });
+//     belleDivDevoe.addEventListener('mouseleave', function () {
+//         belleDivDevoe.innerHTML = "";
+//     });
+//     belleDivDevoe.addEventListener('click', function () {
+//         belleDivDevoe.style.backgroundColor = randomColor();
+
+//         function randomColor() {
+//             var randomRed = Math.floor(Math.random() * 350);
+//             var randomGreen = Math.floor(Math.random() * 350);
+//             var randomBlue = Math.floor(Math.random() * 350);
+//             var randomColor = "rgb(" + randomRed + "," + randomGreen + "," + randomBlue + ")";
+//             return randomColor
+//         }
+//     });
+
+//     belleDivDevoe.addEventListener('dblclick', function () {
+//         if (belleDivDevoe.id % 2 == 0 && belleDivDevoe.nextSibling != null) {
+//             belleDivDevoe.nextSibling.remove();
+//         }
+//         else if (belleDivDevoe.id % 2 == 1 && div.previousSibling != button) {
+//             belleDivDevoe.previousSibling.remove();
+//         }
+//         else {
+//             alert('Element does not exist');
+//         }
+//     })
+//     x.style.float = 'left';
 //     belleDivDevoe.style.backgroundColor = 'black';
-//     belleDivDevoe.style.height = '80px';
+//     belleDivDevoe.style.height = '100px';
 //     belleDivDevoe.style.width = '80px';
+//     belleDivDevoe.style.clear = "both";
 //     x.appendChild(belleDivDevoe);
-//     document.body.appendChild(x);
-// }     
+//     document.body.appendChild(belleDivDevoe);
 
-function pleaseJustWork() {
-    var x = document.createElement('Button');
-    var t = document.createTextNode('Click me');
-    var belleDivDevoe =  document.createElement('div');
+// } 
+
+const rainbow= function() {
+            var randomRed = Math.floor(Math.random() * 350);
+            var randomGreen = Math.floor(Math.random() * 350);
+            var randomBlue = Math.floor(Math.random() * 350);
+            var rainbow = "rgb(" + randomRed + "," + randomGreen + "," + randomBlue + ")";
+            return rainbow
+        }
+var divId = 1;
+var pleaseWork = function(){
+    var belleDivDevoe = document.createElement('div');
     belleDivDevoe.style.backgroundColor = 'black';
-    belleDivDevoe.style.height = '80px';
+    belleDivDevoe.style.height = '100px';
     belleDivDevoe.style.width = '80px';
-    belleDivDevoe.style.float = "left";
-    x.appendChild(belleDivDevoe);
+    belleDivDevoe.style.clear = "both";
+    belleDivDevoe.id = divId;
+    divId++;
+    belleDivDevoe.addEventListener('mouseout',function(){
+        this.innerHTML = "";
+    });
+    belleDivDevoe.addEventListener('mouseover', function () {
+        this.innerHTML = this.id;
+    });
+    belleDivDevoe.addEventListener('dblclick', function(){
+      
+        if ((this.id % 2) === 0 && this.nextSibling != null) {
+            this.nextSibling.remove();
+        }
+        else if ((this.id % 2) == 1 && this.previousSibling != document.getElementById('divBuildBtn')) {
+            this.previousSibling.remove();
+        }
+        else {
+            alert('Element does not exist');
+        }
+    });
+    belleDivDevoe.addEventListener('click', function(){
+      this.style.backgroundColor = rainbow();
+    });
     document.body.appendChild(belleDivDevoe);
-    belleDivDevoe.appendChild(t);
-
-}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
+};
